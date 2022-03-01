fb16f2ac944ab673b13c677179d702de4cec1419
andrewchester1 <49591047+andrewchester1@users.noreply.github.com>
2022-03-01 15:50:22 -0600

    final

--- main.py
+++ main.py
@@ -1,60 +1,122 @@
-# Python program for implementation of MergeSort
-def mergeSort(arr):
-    if len(arr) > 1:
+import timeit
+import random
 
-        # Finding the mid of the array
-        mid = len(arr) // 2
+# # Python program for implementation of MergeSort
+# def mergeSort(arr):
+#     if len(arr) > 1:
+#
+#         # Finding the mid of the array
+#         mid = len(arr) // 2
+#
+#         # Dividing the array elements
+#         L = arr[:mid]
+#
+#         # into 2 halves
+#         R = arr[mid:]
+#
+#         # Sorting the first half
+#         mergeSort(L)
+#
+#         # Sorting the second half
+#         mergeSort(R)
+#
+#         i = j = k = 0
+#
+#         # Copy data to temp arrays L[] and R[]
+#         while i < len(L) and j < len(R):
+#             if L[i] < R[j]:
+#                 arr[k] = L[i]
+#                 i += 1
+#             else:
+#                 arr[k] = R[j]
+#                 j += 1
+#             k += 1
+#
+#         # Checking if any element was left
+#         while i < len(L):
+#             arr[k] = L[i]
+#             i += 1
+#             k += 1
+#
+#         while j < len(R):
+#             arr[k] = R[j]
+#             j += 1
+#             k += 1
+#
+#
+# # Code to print the list
+#
+#
+# def printList(arr):
+#     for i in range(len(arr)):
+#         print(arr[i], end=" ")
+#     print()
+#
+#
+# # Driver Code
+# # if __name__ == '__main__':
+# #     arr = [12, 11, 13, 5, 6, 7]
+# #     print("Given array is", end="\n")
+# #     printList(arr)
+# #     mergeSort(arr)
+# #     print("Sorted array is: ", end="\n")
+# #     printList(arr)
+#
+# def GenerateArray(size):
+#     array = []
+#     for i in range(size):
+#         array.append(i)
+#     random.shuffle(array)
+#     return array
+#
+# def test():
+#     arr = GenerateArray(1000)
+#     mergeSort(arr)
+#
+# print(timeit.Timer(test).timeit(number=10))
 
-        # Dividing the array elements
-        L = arr[:mid]
+def mergeSort(myList):
+    if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
 
-        # into 2 halves
-        R = arr[mid:]
+        mergeSort(left)
+        mergeSort(right)
 
-        # Sorting the first half
-        mergeSort(L)
+        leftIndex = 0
+        rightIndex = 0
 
-        # Sorting the second half
-        mergeSort(R)
+        mainIndex = 0
 
-        i = j = k = 0
-
-        # Copy data to temp arrays L[] and R[]
-        while i < len(L) and j < len(R):
-            if L[i] < R[j]:
-                arr[k] = L[i]
-                i += 1
+        while leftIndex < len(left) and rightIndex < len(right):
+            if left[leftIndex] <= right[rightIndex]:
+                myList[mainIndex] = left[leftIndex]
+                leftIndex += 1
             else:
-                arr[k] = R[j]
-                j += 1
-            k += 1
+                myList[mainIndex] = right[rightIndex]
+                rightIndex += 1
+            mainIndex += 1
 
-        # Checking if any element was left
-        while i < len(L):
-            arr[k] = L[i]
-            i += 1
-            k += 1
+        while leftIndex < len(left):
+            myList[mainIndex] = left[leftIndex]
+            leftIndex += 1
+            mainIndex += 1
 
-        while j < len(R):
-            arr[k] = R[j]
-            j += 1
-            k += 1
+        while rightIndex < len(right):
+            myList[mainIndex] = right[rightIndex]
+            rightIndex += 1
+            mainIndex += 1
 
+def GenerateArray(size):
+    array = []
+    for i in range(size):
+        array.append(i)
+    random.shuffle(array)
+    return array
 
-# Code to print the list
+def test():
+    randmonArray = GenerateArray(1000)
+    mergeSort(randmonArray)
 
-
-def printList(arr):
-    for i in range(len(arr)):
-        print(arr[i], end=" ")
-    print()
-
-
-# Driver Code
-if __name__ == '__main__':
-    arr = [12, 11, 13, 5, 6, 7]
-    print("Given array is", end="\n")
-    printList(arr)
-    mergeSort(arr)
-    print("Sorted array is: ", end="\n")
-    printList(arr)
+print(timeit.Timer(test).timeit(number=10))
